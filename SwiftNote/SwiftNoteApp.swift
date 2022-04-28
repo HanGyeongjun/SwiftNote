b83d942c6dc138a091971949da7462bcd7de9118
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-04-28 16:59:55 +0900

    Initial Commit

---
+++ SwiftNote/SwiftNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNoteApp.swift
+//  SwiftNote
+//
+//  Created by 한경준 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
